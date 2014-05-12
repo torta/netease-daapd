5000047102ea50517d3fdc65eea5c15287d9a840
torta <torta@hloli.net>
2014-05-12 19:38:27 +0800

    remove url from db.json

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var http = require('http');
 var fs = require('fs');
+var ext = require('./lib/ext');
 var db = require('./data/db.json');
 var router = require('./data/routes.json');
 var port = 13689;
 var mdns;
 
 try {
-  var mdns = require('mdns');
+  mdns = require('mdns');
 } catch (e) {
   console.log('not found mdns: npm install mdns');
   process.exit(1);
@@ -17,27 +18,28 @@ http.createServer(function (req, res) {
 
   for (var k in router) {
     if (req.url.indexOf(k) === 0) {
-        fs.readFile(router[k], function(e, d) {
-              res.writeHead(200, {
-                'DAAP-Server': 'daap/1.0',
-                'Content-Encoding': 'gzip',
-                'Content-Type': 'application/x-dmap-tagged',
-                'Content-Length': d.length
-              });
-            res.end(d);
+      fs.readFile(router[k], function(e, d) {
+        res.writeHead(200, {
+          'DAAP-Server': 'daap/1.0',
+          'Content-Encoding': 'gzip',
+          'Content-Type': 'application/x-dmap-tagged',
+          'Content-Length': d.length
         });
-        return;
+        res.end(d);
+      });
+      return;
     }
   }
 
   var item = req.url.match(/\/items\/(\d+)(.)/);
   if (item && db[item[1]]) {
-    var url = db[item[1]].au;
-    if(item[2] == '.') url = db[item[1]].mu;
+    var t = item[2];
+    var e = db[item[1]];
+    var url = t == '.' ? ext.encode_url(e.m, 'm') : ext.encode_url(e.a, 'p');
     res.writeHead(303, {'Location': url});
     return res.end();
   }
 }).listen(port);
 
-mdns.createAdvertisement(mdns.tcp('daap'), port, {name: "netease"}).start();
+mdns.createAdvertisement(mdns.tcp('daap'), port, {name: 'netease'}).start();
 console.log('Server running at port ' + port + ', check your itunes shared library');
